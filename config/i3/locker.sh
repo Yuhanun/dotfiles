02e4513a16c968c7f582df882ff9f9c8e728a6aa
Hans Goor <rooteddev@outlook.com>
2020-04-28 10:55:47 +0200

    Mute mic and speaker on lock.

--- config/i3/locker.sh
+++ config/i3/locker.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-/usr/bin/pactl set-sink-volume 0 0%
+# Mute the speakers.
+/usr/bin/pactl set-sink-volume 0 0
+/usr/bin/pactl set-sink-mute 0 1
+
+# Mute the microphone.
+/usr/bin/pactl set-source-mute @DEFAULT_SOURCE@ 1
+
+# Lock the screen.
 /usr/bin/betterlockscreen --lock blur
